17e1801df59e68339a89f8562a7b5757f5ff51d8
Shamsiddinov Mladshiy <onlystudyacc0unt@gmail.com>
2024-04-18 21:55:44 +0500

    qwerty

---
+++ js/app.js
@@ -0,0 +1,87 @@
+//++++++++++++++++++++++++++1savol+++++++++++++++++++++
+// let n = +prompt(`son kiriting`)
+// if(n % 2 == 0){
+//     alert(`${n} juft son`);
+// }else if (n % 2 == 1){
+//     alert(`qoldigi ${n%2} chiqadi`);
+// }
+
+//++++++++++++++++++++++++++2savol+++++++++++++++++++++
+
+// let n = Math.random()*10
+
+// alert(Math.round(n))
+
+//++++++++++++++++++++++++++3savol+++++++++++++++++++++
+
+// let n = 12.510
+// alert(Math.round(n))
+
+//++++++++++++++++++++++++++4savol+++++++++++++++++++++
+
+// function word(word,times){
+//     for (let i = 0; i < times; i++){
+//         console.log(word);
+//     }
+// }
+// word("Mars It scholl",10)
+
+//++++++++++++++++++++++++++5savol+++++++++++++++++++++
+
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+// console.log("MARS IT SCHOOL")
+
+//++++++++++++++++++++++++++6savol+++++++++++++++++++++
+
+//  let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+//   let ism = "Abdulloh"
+
+//   for (let i = 0; i < ism.length; i++){
+//   let harf = ism [i],toLowerCasel ;
+//   if (harflar.includes (harf)){
+//       console.log(harf);
+//   }
+//   }
+
+//++++++++++++++++++++++++++7savol+++++++++++++++++++++
+
+// let y = +prompt(`yoshingizni kiriting`)
+// if(y > 18){
+//     alert("Siz balag'ot yoshiga yetgansiz")
+// }else if(y < 18){
+//     alert("siz balog'at yoshiga yetmagansiz")
+// }else if(y = 18) {
+//     alert("balog'at yoshi muborak")
+// }
+
+//++++++++++++++++++++++++++8savol+++++++++++++++++++++
+
+// let arr = ['a','b','d','u','l','l','o','h']
+// arr.reverse()
+// console.log(arr)
+
+
+//++++++++++++++++++++++++++10savol+++++++++++++++++++++
+
+// let Array = [1,2,3,4,5,6,7,8,9,10]
+// let odd = []
+// let even = []
+
+// for(let i = 0; i < Array.length; i++){
+//     const element = Array[i]
+//     if (element % 2 == 0){
+//         even.push(element);
+//     }else{
+//         odd.push(Element);
+//     }
+// }
+// console.log(even)
